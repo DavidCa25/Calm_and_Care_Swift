c7c80252cfbab4a399018c57361b9e3a54dd533d
David <banerdavid29@gmail.com>
2024-06-03 17:54:00 -0600

    Initial Commit

---
+++ Calm_and_Care/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calm_and_Care
+//
+//  Created by ISSC_611_2024 on 03/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calm_and_CareUITests/Calm_and_CareUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Calm_and_CareUITestsLaunchTests.swift
+//  Calm_and_CareUITests
+//
+//  Created by ISSC_611_2024 on 03/06/24.
+//
+
+import XCTest
+
+final class Calm_and_CareUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
